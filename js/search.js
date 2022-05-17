225fec2e6a24b8d83f1f6e51e6acf7de143ec3eb
Hernan Esteban Devia Grajales <hernan.devia@GRUPOMAS.COM>
2022-05-17 16:27:08 -0500

    Finish articulo-novedades page; Add articulo-estadistica page

--- js/search.js
+++ js/search.js
@@ -1,13 +1,19 @@
 const check = document.querySelectorAll('input[type="radio"]')
 
 check.forEach(e => {
-    e.addEventListener("click", function() {
+    e.addEventListener("click", function () {
         const results = document.querySelector(".results")
-        if(e.id == "grid"){
-            results.classList.add('twoColumns')                
+        if (e.id == "grid") {
+            results.classList.add('twoColumns')
         }
         else {
             results.classList.remove('twoColumns')
         }
     })
+})
+
+const filter = document.querySelector("#filterSearch")
+filter.addEventListener("click", function () {
+    const filtermob = document.querySelector(".filtersearch")
+    filtermob.style.display = "block"
 })
